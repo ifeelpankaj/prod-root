c9141db1ebe5751ffbac6dedbe41ea446183c3fb
ifeelpankaj <ifeelpankaj@gmail.com>
2025-03-10 00:47:09 +0530

    Ci: Configure Webhook

--- prod-client/src/__components__/header/header.jsx
+++ prod-client/src/__components__/header/header.jsx
@@ -82,7 +82,7 @@ const Header = () => {
                             src={Logo}
                             className="header__logo-icon"
                         />
-                        <span className="header__logo-text">Velocity Rides</span>
+                        <span className="header__logo-text">Undefined Rides</span>
                     </Link>
                 )}
 
