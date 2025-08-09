11ff7bb414505ddee18a47f8ad41aaddebe4c703
ifeelpankaj <ifeelpankaj@gmail.com>
2025-08-09 16:57:57 +0530

    fix: Added domain-2

--- prod-admin/vite.config.js
+++ prod-admin/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    preview: {
+        allowedHosts: ['4biddencoder.tech', 'www.4biddencoder.tech', 'admin.4biddencoder.tech'],
+        port: 4174 // or 4174 for admin app
+    }
 });
