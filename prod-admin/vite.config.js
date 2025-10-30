aa29fa831f5abe6c8761b94c7c9cb5f2274b098a
ifeelpankaj <ifeelpankaj@gmail.com>
2025-10-30 14:25:52 +0530

    ci: Added Nginx Config-final-may-be

--- prod-admin/vite.config.js
+++ prod-admin/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
     plugins: [react()],
     preview: {
-        allowedHosts: ['4biddencoder.tech', 'www.4biddencoder.tech', 'admin.4biddencoder.tech'],
+        allowedHosts: ['cabservice-admin.4biddencoder.tech', 'localhost', '127.0.0.1'],
         port: 4174 // or 4174 for admin app
     }
 });
--- prod-client/vite.config.js
+++ prod-client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
     plugins: [react()],
     preview: {
-        allowedHosts: ['4biddencoder.tech', 'www.4biddencoder.tech', 'admin.4biddencoder.tech'],
+        allowedHosts: ['cabservice-app.4biddencoder.tech', 'localhost', '127.0.0.1'],
         port: 4173 // or 4174 for admin app
     }
 });
